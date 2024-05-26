a4e2be866a27818a34ed4a81845ff70f607c463b
Ret-Mode <ernest.harcej@wp.pl>
2024-05-26 14:14:48 +0200

    Removing sdl from glfw

--- backends/glfw_backend/include/sdl_keys.h
+++ backends/glfw_backend/include/sdl_keys.h
@@ -4,96 +4,91 @@
 #ifndef KEYS_LOADED
 #define KEYS_LOADED
 
-#ifdef _WIN32
-#include "SDL.h"
-//#include "SDL_syswm.h"
-#endif
-#if !defined(_WIN32)
-#include <SDL/SDL.h>
-#endif
+#define GLFW_DLL
+#include <GLFW/glfw3.h>
 
 typedef int     ScanCode;
 
 #define sc_None         0
 #define sc_Bad          0xff
-#define sc_Return       SDLK_RETURN
+#define sc_Return       GLFW_KEY_ENTER
 #define sc_Enter        sc_Return
-#define sc_Escape       SDLK_ESCAPE
-#define sc_Space        SDLK_SPACE
-#define sc_BackSpace    SDLK_BACKSPACE
-#define sc_Tab          SDLK_TAB
-#define sc_Alt          SDLK_LALT
-#define sc_Control      SDLK_LCTRL
-#define sc_CapsLock     SDLK_CAPSLOCK
-#define sc_LShift       SDLK_LSHIFT
-#define sc_RShift       SDLK_RSHIFT
-#define sc_UpArrow      SDLK_UP
-#define sc_DownArrow    SDLK_DOWN
-#define sc_LeftArrow    SDLK_LEFT
-#define sc_RightArrow   SDLK_RIGHT
-#define sc_Insert       SDLK_INSERT
-#define sc_Delete       SDLK_DELETE
-#define sc_Home         SDLK_HOME
-#define sc_End          SDLK_END
-#define sc_PgUp         SDLK_PAGEUP
-#define sc_PgDn         SDLK_PAGEDOWN
-#define sc_Keypad5      SDLK_KP5
-#define sc_F1           SDLK_F1
-#define sc_F2           SDLK_F2
-#define sc_F3           SDLK_F3
-#define sc_F4           SDLK_F4
-#define sc_F5           SDLK_F5
-#define sc_F6           SDLK_F6
-#define sc_F7           SDLK_F7
-#define sc_F8           SDLK_F8
-#define sc_F9           SDLK_F9
-#define sc_F10          SDLK_F10
-#define sc_F11          SDLK_F11
-#define sc_F12          SDLK_F12
+#define sc_Escape       GLFW_KEY_ESCAPE
+#define sc_Space        GLFW_KEY_SPACE
+#define sc_BackSpace    GLFW_KEY_BACKSPACE
+#define sc_Tab          GLFW_KEY_TAB
+#define sc_Alt          GLFW_KEY_LEFT_ALT
+#define sc_Control      GLFW_KEY_LEFT_CONTROL
+#define sc_CapsLock     GLFW_KEY_CAPSLOCK
+#define sc_LShift       GLFW_KEY_LEFT_SHIFT
+#define sc_RShift       GLFW_KEY_RIGHT_SHIFT
+#define sc_UpArrow      GLFW_KEY_UP
+#define sc_DownArrow    GLFW_KEY_DOWN
+#define sc_LeftArrow    GLFW_KEY_LEFT
+#define sc_RightArrow   GLFW_KEY_RIGHT
+#define sc_Insert       GLFW_KEY_INSERT
+#define sc_Delete       GLFW_KEY_DELETE
+#define sc_Home         GLFW_KEY_HOME
+#define sc_End          GLFW_KEY_END
+#define sc_PgUp         GLFW_KEY_PAGE_UP
+#define sc_PgDn         GLFW_KEY_PAGE_DOWN
+#define sc_Keypad5      GLFW_KEY_KP_5
+#define sc_F1           GLFW_KEY_F1
+#define sc_F2           GLFW_KEY_F2
+#define sc_F3           GLFW_KEY_F3
+#define sc_F4           GLFW_KEY_F4
+#define sc_F5           GLFW_KEY_F5
+#define sc_F6           GLFW_KEY_F6
+#define sc_F7           GLFW_KEY_F7
+#define sc_F8           GLFW_KEY_F8
+#define sc_F9           GLFW_KEY_F9
+#define sc_F10          GLFW_KEY_F10
+#define sc_F11          GLFW_KEY_F11
+#define sc_F12          GLFW_KEY_F12
 
-#define sc_ScrollLock       SDLK_SCROLLOCK
-#define sc_PrintScreen      SDLK_PRINT
+#define sc_ScrollLock       GLFW_KEY_SCROLL_LOCK
+#define sc_PrintScreen      GLFW_KEY_PRINT_SCREEN
 
-#define sc_1            SDLK_1
-#define sc_2            SDLK_2
-#define sc_3            SDLK_3
-#define sc_4            SDLK_4
-#define sc_5            SDLK_5
-#define sc_6            SDLK_6
-#define sc_7            SDLK_7
-#define sc_8            SDLK_8
-#define sc_9            SDLK_9
-#define sc_0            SDLK_0
+#define sc_1            GLFW_KEY_1
+#define sc_2            GLFW_KEY_2
+#define sc_3            GLFW_KEY_3
+#define sc_4            GLFW_KEY_4
+#define sc_5            GLFW_KEY_5
+#define sc_6            GLFW_KEY_6
+#define sc_7            GLFW_KEY_7
+#define sc_8            GLFW_KEY_8
+#define sc_9            GLFW_KEY_9
+#define sc_0            GLFW_KEY_0
 
-#define sc_A            SDLK_a
-#define sc_B            SDLK_b
-#define sc_C            SDLK_c
-#define sc_D            SDLK_d
-#define sc_E            SDLK_e
-#define sc_F            SDLK_f
-#define sc_G            SDLK_g
-#define sc_H            SDLK_h
-#define sc_I            SDLK_i
-#define sc_J            SDLK_j
-#define sc_K            SDLK_k
-#define sc_L            SDLK_l
-#define sc_M            SDLK_m
-#define sc_N            SDLK_n
-#define sc_O            SDLK_o
-#define sc_P            SDLK_p
-#define sc_Q            SDLK_q
-#define sc_R            SDLK_r
-#define sc_S            SDLK_s
-#define sc_T            SDLK_t
-#define sc_U            SDLK_u
-#define sc_V            SDLK_v
-#define sc_W            SDLK_w
-#define sc_X            SDLK_x
-#define sc_Y            SDLK_y
-#define sc_Z            SDLK_z
+#define sc_A            GLFW_KEY_A
+#define sc_B            GLFW_KEY_B
+#define sc_C            GLFW_KEY_C
+#define sc_D            GLFW_KEY_D
+#define sc_E            GLFW_KEY_E
+#define sc_F            GLFW_KEY_F
+#define sc_G            GLFW_KEY_G
+#define sc_H            GLFW_KEY_H
+#define sc_I            GLFW_KEY_I
+#define sc_J            GLFW_KEY_J
+#define sc_K            GLFW_KEY_K
+#define sc_L            GLFW_KEY_L
+#define sc_M            GLFW_KEY_M
+#define sc_N            GLFW_KEY_N
+#define sc_O            GLFW_KEY_O
+#define sc_P            GLFW_KEY_P
+#define sc_Q            GLFW_KEY_Q
+#define sc_R            GLFW_KEY_R
+#define sc_S            GLFW_KEY_S
+#define sc_T            GLFW_KEY_T
+#define sc_U            GLFW_KEY_U
+#define sc_V            GLFW_KEY_V
+#define sc_W            GLFW_KEY_W
+#define sc_X            GLFW_KEY_X
+#define sc_Y            GLFW_KEY_Y
+#define sc_Z            GLFW_KEY_Z
 
 #define key_None        0
-#define sc_Last         SDLK_LAST
+#define sc_Last         GLFW_KEY_LAST
 
 const char *GetScanName (ScanCode scan);
 
--- backends/glfw_backend/src/glfw_impl.cpp
+++ backends/glfw_backend/src/glfw_impl.cpp
@@ -31,42 +31,37 @@ static struct wolfColor_t gamepal[]={
 typedef struct Vertex
 {
     float pos[2];
-    float col[3];
 } Vertex;
  
 static const Vertex vertices[3] =
 {
-    { { -0.6f, -0.4f }, { 1.f, 0.f, 0.f } },
-    { {  0.6f, -0.4f }, { 0.f, 1.f, 0.f } },
-    { {   0.f,  0.6f }, { 0.f, 0.f, 1.f } }
+    { { -0.6f, -0.4f } },
+    { {  0.6f, -0.4f } },
+    { {   0.f,  0.6f } }
 };
  
 static GLuint paletteTexture;
 static GLuint imageTexture; 
 
-static const char* vertex_shader_text =
+static const char *vertSource = 
 "#version 330\n"
-"uniform mat4 MVP;\n"
-"in vec3 vCol;\n"
-"in vec2 vPos;\n"
-"out vec2 pPos;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    pPos = vPos;\n"
-"}\n";
- 
-static const char* fragment_shader_text =
+"layout(location=0)in vec2 vPos;\n"
+"out vec2 pictPos;\n"
+"void main() {\n"
+"  gl_Position = vec4(vPos, 0.0, 1.0);\n"
+"  pictPos = vPos / 2.0 + vec2(0.5, 0.5);\n"
+"}";
+
+static const char *fragSource = 
 "#version 330\n"
-"in vec2 pPos;\n"
-"uniform sampler1D paletteTexture;\n"
-"uniform usampler2D imageTexture;\n"
-"out vec4 fragment;\n"
-"void main()\n"
-"{\n"
-"uvec4 coords = texture(imageTexture,pPos);\n"
-"    fragment = texture(paletteTexture, float(pPos.x));\n"
-"}\n";
+"in vec2 pictPos;\n"
+"uniform usampler1D paletteTexture;\n"
+"uniform sampler2D imageTexture;\n"
+"out vec4 color;\n"
+"void main() {\n"
+"  vec4 color1 = texture(imageTexture, pictPos);\n"
+"  color = texture(paletteTexture, color1.r)/255.0;\n"
+"}";
  
 static void error_callback(int error, const char* description)
 {
@@ -108,7 +103,6 @@ int initGlfw(void)
     //gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
     glfwSwapInterval(1);
  
-    // NOTE: OpenGL error checks have been omitted for brevity
  
     /* create palette texture */
     paletteTexture;
@@ -116,7 +110,13 @@ int initGlfw(void)
     glGenTextures(1, &paletteTexture);
     
     glBindTexture(GL_TEXTURE_1D, paletteTexture);
-    glTexImage1D(GL_TEXTURE_1D, 0, GL_RGBA, 256, 0, GL_RGBA, GL_UNSIGNED_BYTE, gamepal);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glTexImage1D(GL_TEXTURE_1D, 0, GL_RGBA8UI, 256, 0, GL_RGBA_INTEGER, GL_UNSIGNED_BYTE, gamepal);
+    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     GLenum glerr;
 
     /* create image texture */
@@ -128,8 +128,11 @@ int initGlfw(void)
     for (int i = 0; i < 320*200; ++i) {
         tempData[i] = 20;
     }
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_R8UI, 320, 200, 0, GL_RED_INTEGER, GL_UNSIGNED_BYTE, tempData);
     free(tempData);
 
@@ -139,7 +142,7 @@ int initGlfw(void)
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
  
     const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+    glShaderSource(vertex_shader, 1, &vertSource, NULL);
     glCompileShader(vertex_shader);
     {
         GLint isCompiled = 0;
@@ -158,7 +161,7 @@ int initGlfw(void)
     }
  
     const GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+    glShaderSource(fragment_shader, 1, &fragSource, NULL);
     glCompileShader(fragment_shader);
  
     {
@@ -181,7 +184,7 @@ int initGlfw(void)
     glAttachShader(program, vertex_shader);
     glAttachShader(program, fragment_shader);
     glLinkProgram(program);
-
+    glUseProgram(program);
     {
         GLint isLinked = 0;
         glGetProgramiv(program, GL_LINK_STATUS, &isLinked);
@@ -199,9 +202,7 @@ int initGlfw(void)
         }
     }
 
-    const GLint mvp_location = glGetUniformLocation(program, "MVP");
     const GLint vpos_location = glGetAttribLocation(program, "vPos");
-    const GLint vcol_location = glGetAttribLocation(program, "vCol");
     const GLint paletteTexture_location = glGetUniformLocation(program, "paletteTexture");
     if (paletteTexture_location) {
         glUniform1i(paletteTexture_location, 1);
@@ -216,10 +217,7 @@ int initGlfw(void)
     glBindVertexArray(vertex_array);
     glEnableVertexAttribArray(vpos_location);
     glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-                          sizeof(Vertex), (void*) offsetof(Vertex, pos));
-    glEnableVertexAttribArray(vcol_location);
-    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-                          sizeof(Vertex), (void*) offsetof(Vertex, col));
+                          sizeof(Vertex), NULL);
  
 
     int width, height;
@@ -228,32 +226,13 @@ int initGlfw(void)
 
     glViewport(0, 0, width, height);
     glClear(GL_COLOR_BUFFER_BIT);
-
-    float mvp[16];
-    mvp[0] = 1.0f;
-    mvp[1] = 0.0f;
-    mvp[2] = 0.0f;
-    mvp[3] = 0.0f;
-    mvp[4] = 0.0f;
-    mvp[5] = 1.0f;
-    mvp[6] = 0.0f;
-    mvp[7] = 0.0f;
-    mvp[8] = 0.0f;
-    mvp[9] = 0.0f;
-    mvp[10] = 1.0f;
-    mvp[11] = 0.0f;
-    mvp[12] = 0.0f;
-    mvp[13] = 0.0f;
-    mvp[14] = 0.0f;
-    mvp[15] = 1.0f;
     
-
-    glUseProgram(program);
-    glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) &mvp);
     glBindVertexArray(vertex_array);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
+
     glfwSwapBuffers(window);
+
 
     return 0;
 }
@@ -261,287 +240,286 @@ int initGlfw(void)
 /* IMPL ******************************************************************/
 extern void *_GetLatchPic(int which);
 void *GetLatchPic(int which) {
-    return _GetLatchPic(which);
+    return NULL;
 }
 
 extern void _SetLatchPic(int which, void *data);
 void SetLatchPic(int which, void *data) {
-    _SetLatchPic(which, data);
+    ;
 }
 
 extern int _GetLatchPicWidth(int which);
 int GetLatchPicWidth(int which) {
-    return _GetLatchPicWidth(which);
+    return 0;
 }
 
 extern int _GetLatchPicHeight(int which);
 int GetLatchPicHeight(int which) {
-    return _GetLatchPicHeight(which);
+    return 0;
 }
 
 extern void _DelayWolfTicks(int ticks);
 void DelayWolfTicks(int ticks) {
-    _DelayWolfTicks(ticks);
+    ;
 }
 
 extern void _DelayMilliseconds(int milliseconds);
 void DelayMilliseconds(int milliseconds) {
-    _DelayMilliseconds(milliseconds);
+    ;
 }
 
 extern void _DelayVBL(int param);
 void DelayVBL(int param) {
 
-    _DelayVBL(param);
+    ;
 }
 
 extern unsigned int _GetWolfTicks(void); 
 unsigned int GetWolfTicks(void) {
-    return _GetWolfTicks();
+    return 0;
 }
 
 extern unsigned int _GetMilliseconds(void);
 unsigned int GetMilliseconds(void) {
-    return _GetMilliseconds();
+    return 0;
 }
 
 extern unsigned char *_GraphicLockBytes(void *surface);
 unsigned char *GraphicLockBytes(void *surface)
 {
-    return _GraphicLockBytes(surface);
+    return NULL;
 }
 
 extern void _GraphicUnlockBytes(void *surface);
 void GraphicUnlockBytes(void *surface)
 {
-    _GraphicUnlockBytes(surface);
+    ;
 }
 
 extern void *_CreateScreenBuffer(void *gamepal, unsigned int *bufferPitch, unsigned int screenWidth, unsigned int screenHeight);
 void *CreateScreenBuffer(void *gamepal, unsigned int *bufferPitch, unsigned int screenWidth, unsigned int screenHeight) {
-    return _CreateScreenBuffer(gamepal, bufferPitch, screenWidth, screenHeight);
+    return NULL;
 }
 
 extern void *_GetScreenBuffer(void);
 void *GetScreenBuffer(void) {
-    return _GetScreenBuffer();
+    return NULL;
 }
 
 extern unsigned char _GetScreenBufferPixel(int offset);
 unsigned char GetScreenBufferPixel(int offset) {
-    return _GetScreenBufferPixel(offset);
+    return 0;
 }
 
 extern void _GetCurrentPaletteColor(int color, int *red, int *green, int *blue);
 void GetCurrentPaletteColor(int color, int *red, int *green, int *blue) {
-    _GetCurrentPaletteColor(color, red, green, blue);
+    ;
 }
 
 extern void _SetCurrentPaletteColor(int color, int red, int green, int blue, unsigned int screenBits);
 void SetCurrentPaletteColor(int color, int red, int green, int blue, unsigned int screenBits) {
-    _SetCurrentPaletteColor(color, red, green, blue, screenBits);
+    ;
 }
 
 extern void _SetWholePalette(void *palette, int forceupdate);
 void SetWholePalette(void *palette, int forceupdate) {
-    _SetWholePalette(palette, forceupdate);
+    ;
 }
 
 extern void _ConvertPalette(unsigned char *srcpal, void *dest, int numColors);
 void ConvertPalette(unsigned char *srcpal, void *dest, int numColors) {
-    _ConvertPalette(srcpal, dest, numColors);
+    ;
 }
 
 extern void _FillPalette(int red, int green, int blue);
 void FillPalette(int red, int green, int blue) {
-    _FillPalette(red, green, blue);
+    ;
 }
 
 extern void _GetWholePalette(void *palette);
 void GetWholePalette(void *palette) {
-    _GetWholePalette(palette);
+    ;
 }
 
 extern void _SetScreenPalette(void);
 void SetScreenPalette(void) {
-    _SetScreenPalette();
+    ;
 }
 
 extern void _SetWindowTitle(const char *title);
 void SetWindowTitle(const char *title) {
-    _SetWindowTitle(title);
+    ;
 }
 
 extern void _SetScreenBits(void);
 void SetScreenBits(void) {
-    _SetScreenBits(); 
+    ; 
 }
 
 extern unsigned _GetScreenBits(void);
 unsigned GetScreenBits(void) {
-    return _GetScreenBits();
+    return 0;
 }
 
 extern void _SetScreen(void *screenPtr);
 void SetScreen(void *screenPtr) {
-    _SetScreen(screenPtr);
+    ;
 }
 
 extern void *_GetScreen(void);
 void *GetScreen(void) {
-    return _GetScreen();
+    return NULL;
 }
 
 extern short _GetScreenFlags(void);
 short GetScreenFlags(void) {
-    return _GetScreenFlags();
+    return NULL;
 }
 
 extern unsigned short _GetScreenPitch(void);
 unsigned short GetScreenPitch(void) {
-    return _GetScreenPitch();
+    return 0;
 }
 
 extern void *_GetScreenFormat(void);
 void *GetScreenFormat(void) {
-    return _GetScreenFormat();
+    return NULL;
 }
 
 extern unsigned char _GetScreenBytesPerPixel(void);
 unsigned char GetScreenBytesPerPixel(void) {
-    return _GetScreenBytesPerPixel();
+    return 0;
 }
 
 extern void *_GetCurSurface(void);
 void *GetCurSurface(void) {
-    return _GetCurSurface();
+    return NULL;
 }
 
 extern void _SetCurSurface(void *current);
 void SetCurSurface(void *current) {
-    return _SetCurSurface(current);
+    ;
 }
 
 extern unsigned char *_GetCurSurfacePixels(void);
 unsigned char *GetCurSurfacePixels(void) {
-    return _GetCurSurfacePixels();
+    return NULL;
 }
 
 extern void _ClearCurrentSurface(unsigned int color);
 void ClearCurrentSurface(unsigned int color) {
-    return _ClearCurrentSurface(color);
+   ;
 }
 
 extern unsigned char *_GetSurfacePixels(void *surface);
 unsigned char *GetSurfacePixels(void *surface) {
-    return _GetSurfacePixels(surface);
+    return NULL;
 }
 
 extern unsigned short _GetSurfacePitch(void *surface);
 unsigned short GetSurfacePitch(void *surface) {
-    return _GetSurfacePitch(surface);
+    return 0;
 }
 
 extern void *_GetGamePal(void);
 void *GetGamePal(void) {
-    return _GetGamePal();
+    return NULL;
 }
 
 extern void _CenterWindow(void);
 void CenterWindow(void) {
-    _CenterWindow();
+    ;
 }
 
 extern void _ConvertPaletteToRGB(unsigned char *pixelPointer, int width, int height);
 void ConvertPaletteToRGB(unsigned char *pixelPointer, int width, int height) {
-    _ConvertPaletteToRGB(pixelPointer, width, height);
+    ;
 }
 
 extern void _ScreenToScreen (void *source, void *dest);
 void ScreenToScreen (void *source, void *dest) {
-    _ScreenToScreen (source, dest);
+    ;
 }
 
 extern void _LatchToScreenScaledCoord(int which, int xsrc, int ysrc, int width, int height, int scxdest, int scydest);
 void LatchToScreenScaledCoord(int which, int xsrc, int ysrc, int width, int height, int scxdest, int scydest) {
-    _LatchToScreenScaledCoord(which, xsrc, ysrc, width, height, scxdest, scydest);
+    ;
 }
 
 extern void _InitGraphics(void);
 void InitGraphics(void) {
-    _InitGraphics();
     initGlfw();
     atexit(glfwTerminate);
 }
 
 extern void _ReadMouseState(int *btns, int *mx, int *my);
 void ReadMouseState(int *btns, int *mx, int *my) {
-    _ReadMouseState(btns, mx, my);
+    ;
 }
 
 extern void _CenterMouse(int width, int height);
 void CenterMouse(int width, int height) {
-    _CenterMouse(width, height);
+    ;
 }
 
 extern void _InitRedShifts (void);
 void InitRedShifts (void) {
-    _InitRedShifts ();
+    ;
 }
 
 extern void _InitWhiteShifts (void);
 void InitWhiteShifts (void) {
-    _InitWhiteShifts ();
+    ;
 }
 
 extern int _GetWhitePaletteShifts(void);
 int GetWhitePaletteShifts(void) {
-    return _GetWhitePaletteShifts();
+    return 0;
 }
 
 extern int _GetRedPaletteShifts(void);
 int GetRedPaletteShifts(void) {
-    return _GetRedPaletteShifts();
+    return 0;
 }
 
 extern int _GetWhitePaletteSwapMs(void);
 int GetWhitePaletteSwapMs(void) {
-    return _GetWhitePaletteSwapMs();
+    return 0;
 }
 
 extern void* _GetRedPaletteShifted(int which);
 void* GetRedPaletteShifted(int which) {
-    return _GetRedPaletteShifted(which);
+    return NULL;
 }
 
 extern void* _GetWhitePaletteShifted(int which);
 void* GetWhitePaletteShifted(int which) {
-    return _GetWhitePaletteShifted(which);
+    return NULL;
 }
 
 extern void _PaletteFadeOut (int start, int end, int red, int green, int blue, int steps);
 void PaletteFadeOut (int start, int end, int red, int green, int blue, int steps) {
-    _PaletteFadeOut (start, end, red, green, blue, steps);
+    ;
 }
 
 extern void _PaletteFadeIn(int start, int end, void *platettePtr, int steps);
 void PaletteFadeIn(int start, int end, void *platettePtr, int steps) {
-    _PaletteFadeIn(start, end, platettePtr, steps);
+    ;
 }
 
 extern void _SaveBitmap(char *filename);
 void SaveBitmap(char *filename) {
-    _SaveBitmap(filename);
+    ;
 }
 
 extern int _GetMouseButtons(void);
 int GetMouseButtons(void) {
-    return _GetMouseButtons();
+    return 0;
 }
 
 extern int _GetNuberOfJoysticks(void);
 int GetNuberOfJoysticks(void) {
-    return _GetNuberOfJoysticks();
+    return 0;
 }
 
 extern void _SetVGAMode(unsigned *scrWidth, unsigned *scrHeight, 
@@ -550,15 +528,12 @@ extern void _SetVGAMode(unsigned *scrWidth, unsigned *scrHeight,
 void SetVGAMode(unsigned *scrWidth, unsigned *scrHeight, 
                 unsigned *scrPitch, unsigned *bufPitch, 
                 unsigned *currPitch, unsigned *sclFactor) {
-    _SetVGAMode(scrWidth, scrHeight, 
-                scrPitch, bufPitch, 
-                currPitch, sclFactor);
-    
+                    ;
 }
 
 extern void _LoadLatchMemory (void);
 void LoadLatchMemory (void) {
-    _LoadLatchMemory ();
+    ;
 }
 
 extern int _SubFizzleFade (void *src, int x1, int y1,
@@ -569,43 +544,51 @@ int SubFizzleFade (void *src, int x1, int y1,
                        unsigned width, unsigned height, 
                        unsigned frames, int abortable,
                        int rndbits_y, int rndmask){
-    return _SubFizzleFade(src, x1, y1,
-                       width, height, 
-                       frames, abortable,
-                       rndbits_y, rndmask);
+    return 0;
 }
 
 extern void _GetJoystickDelta(int *dx, int *dy);
 void GetJoystickDelta(int *dx, int *dy) {
-    _GetJoystickDelta(dx, dy);
+    ;
 }
 
 extern void _GetJoystickFineDelta(int *dx, int *dy);
 void GetJoystickFineDelta(int *dx, int *dy) {
-    _GetJoystickFineDelta(dx, dy);
+    ;
 }
 
 extern int _GetJoystickButtons(void);
 int GetJoystickButtons(void) {
-    return _GetJoystickButtons();
+    return 0;
 }
 
 extern int _IsJoystickPresent(void);
 int IsJoystickPresent(void) {
-    return _IsJoystickPresent();
+    return 0;
 }
 
-extern void _WaitAndProcessEvents(void);
+extern void ProcessEvents(void);
 void WaitAndProcessEvents(void) {
-    _WaitAndProcessEvents();
+    if (window) {
+        glfwWaitEvents();
+        if(!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+            glfwSwapBuffers(window);
+        }
+        else {
+            glfwDestroyWindow(window);
+            window = NULL;
+        }
+    }
 }
 
-extern void _ProcessEvents(void);
+
 void ProcessEvents(void) {
-    _ProcessEvents();
     if (window) {
         if(!glfwWindowShouldClose(window)) {
             glfwPollEvents();
+            glDrawArrays(GL_TRIANGLES, 0, 3);
             glfwSwapBuffers(window);
         }
         else {
@@ -617,23 +600,23 @@ void ProcessEvents(void) {
 
 extern int _IsInputGrabbed(void);
 int IsInputGrabbed(void) {
-    return _IsInputGrabbed();
+    return 0;
 }
 
 extern void _JoystickShutdown(void);
 void JoystickShutdown(void) {
-    _JoystickShutdown();
+    ;
 }
 
 extern void _JoystickStartup(void);
 void JoystickStartup(void) {
-    _JoystickStartup();
+    ;
 }
 
 
 extern void _CheckIsJoystickCorrect(void);
 void CheckIsJoystickCorrect(void) {
-    _CheckIsJoystickCorrect();
+    ;
 }
 
 
@@ -641,10 +624,75 @@ void CheckIsJoystickCorrect(void) {
 
 
 
-void _VL_MemToScreenScaledCoord (unsigned char *source, int width, int height, int destx, int desty)
+void VL_MemToScreenScaledCoord (unsigned char *source, int width, int height, int destx, int desty)
 {
+    unsigned char *pixels = (unsigned char *)malloc(width * height);
+    for (int j = 0; j < height; ++j) {
+        for (int i = 0; i < width; ++i) {
+            int foffset = ((i & 3) * (width >> 2) * height) + (i >> 2) + j * (width >>2);
+            pixels[(height - j - 1) * width + i] = source[foffset];
+       }
+    }
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
     glActiveTexture(GL_TEXTURE2);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_R8UI, width, height, 0, GL_RED_INTEGER, GL_UNSIGNED_BYTE, source);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_R8UI, width, height, 0, GL_RED_INTEGER, GL_UNSIGNED_BYTE, pixels);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    glGenerateMipmap(GL_TEXTURE_2D);
     glActiveTexture(GL_TEXTURE0);
+    free(pixels);
 
+
+}
+
+
+
+void VL_Plot (int x, int y, int color)
+{
+    ;
+}
+
+
+unsigned char VL_GetPixel (int x, int y)
+{
+    return 0;
+}
+
+void VL_Hlin (unsigned x, unsigned y, unsigned width, int color)
+{
+    ;
+}
+
+void VL_Vlin (int x, int y, int height, int color)
+{
+    ;
+}
+
+
+void VL_BarScaledCoord (int scx, int scy, int scwidth, int scheight, int color)
+{
+    ;
+}
+
+
+void VL_MemToLatch(unsigned char *source, int width, int height,
+    void *destSurface, int x, int y)
+{
+    ;
+}
+
+void VL_MemToScreenScaledCoord (unsigned char *source, int origwidth, int origheight, int srcx, int srcy,
+                                int destx, int desty, int width, int height)
+{
+    ;
+}
+
+
+void VL_LatchToScreenScaledCoord(int which, int xsrc, int ysrc,
+    int width, int height, int scxdest, int scydest)
+{
+    ;
 }
